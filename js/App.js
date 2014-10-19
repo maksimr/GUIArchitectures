ff7fd008276a8765e91c3db47b7268233b1c4b21
Maksim Ryzhikov <rv.maksim@gmail.com>
2014-10-20 01:02:03 +0400

    chore: input control

--- js/App.js
+++ js/App.js
@@ -1,17 +1,56 @@
+/**
+ * @description
+ * Element which allow control volume
+ */
+
+/**
+ * Application record
+ * Business logic of our application
+ * @type {{volume: number}}
+ */
+global.VolumeModel = {
+    volume: 10
+};
+
+/**
+ * Get model object
+ * @param modelName
+ * @returns {*}
+ */
+var getModel = function (modelName) {
+    return global[modelName];
+};
+
+/**
+ * Get field from model
+ * @param model
+ * @param property
+ * @returns {*}
+ */
+var getValueFromModel = function (model, property) {
+    property = [].concat(property);
+    return property.reduce(function (value, propName) {
+        return value[propName]
+    }, model);
+};
+
+/**
+ * Screen records/components
+ */
 var $ = require('jquery');
 
-$(function () {
-    var stationListNode = $('#stationList');
-    var stations = [
-        {
-            value: '1'
-        },
-        {
-            value: '2'
-        }
-    ];
+var InputControl = function (rootNode) {
+    var $rootNode = $(rootNode);
+    var modelData = $rootNode.attr('data-model').split('.');
+    var modelName = modelData.shift();
 
-    stationListNode.append(stations.map(function (station) {
-        return $('<option>' + station.value + '</option>').get(0);
-    }));
-});
+    var model = getModel(modelName);
+
+    /**
+     * View
+     */
+    var modelValue = getValueFromModel(model, modelData);
+    $rootNode.val(modelValue); // Render model value on the screen
+};
+
+console.log(new InputControl($('.input-control').get(0)));
